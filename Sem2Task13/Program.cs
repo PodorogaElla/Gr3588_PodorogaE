d52cb7ba5b0874a6cc423204221ff2cf0308ebd0
PodorogaElla <Elka_forest@mail.ru>
2022-12-08 13:43:00 +0500

    add solution for task 10, 13, 15

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿//Считываем данные с консоли,парсим, проверяем на наличие значения.
+int number1 = int.Parse(Console.ReadLine()??"0");
+
+//Проверяем переменную на трехзначность
+if (number1>99)
+{
+    //Выделяем третью цифру
+    char[] digits = number1.ToString().ToCharArray();
+
+    //Выводим данные в консоль
+    Console.WriteLine("Третья цифра"  + digits[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
