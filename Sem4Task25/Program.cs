491b2b2349c91efc83e04ce68f92d901b3c34c50
PodorogaElla <Elka_forest@mail.ru>
2022-12-15 17:07:26 +0500

    add solution for task 25, 27, 29

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,31 @@
+﻿//запрос данных у пользователя
+int a = ReadData("Введите число А:");
+int b = ReadData("Введите число B:");
+int result = Pow(a,b);
+// Метод вычисления результата
+int Pow(int a, int b)
+{
+    int res =1;
+    for(int i=1;i<=b;i++)
+    {
+        res = a * res;
+    }
+    return res;  
+}
+// Печать результата
+PrintData("А в степени В равно " + result );
+
+int ReadData(string msg)
+{
+    Console.WriteLine (msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+
+
+
+
---
+++ Sem4Task27/Program.cs
@@ -0,0 +1,31 @@
+﻿int number = ReadData("Введите число А:");
+int sum = SumDigit1(number);
+PrintData("Сумма цифр в числе равна " + sum);
+// Программа считывает число:
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+// Программа выдает число:
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+// Метод вычисления:
+int SumDigit1(int num)
+{    
+    int res = 0;
+    while(num>0)
+    {
+        res+=num%10;
+        num=num/10;
+    }
+    return res;
+}
+// Подсчет затраченного на вычисление времени:
+DateTime d1 = DateTime.Now;
+Console.WriteLine(DateTime.Now - d1);
+
+
+
