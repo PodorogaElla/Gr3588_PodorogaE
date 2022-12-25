030f00cfdeebee32981880d809abe0afdabc2a41
PodorogaElla <Elka_forest@mail.ru>
2022-12-26 00:03:34 +0500

    add solution for Tasks 47, 50, 52

--- Sem5Task38/Program.cs
+++ Sem5Task38/Program.cs
@@ -1,51 +1,108 @@
-﻿// Задайте массив вещественных чисел. Найдите разницу 
-// между максимальным и минимальным элементов массива.
+﻿// Неправильный вариант
 
+// Задайте массив вещественных чисел. Найдите разницу 
+// // между максимальным и минимальным элементов массива.
+
+// double[] array = GenArray(5,100,999);
+// PrintArray(array);
+// double maxValue =  double.MinValue; 
+// double minValue =  double.MaxValue; 
+// double result = MinMax(maxValue-minValue);
+// Console.WriteLine();
+// PrintData("Разница между макс и мин элементами массива ", result);
+
+// // Заполняем массив
+// double[] GenArray(int len, int minValue, int maxValue)
+// {
+//     Random rnd = new Random();
+//     double[] outArray = new double[len];
+//     for (int i = 0; i < len; i++)
+//     {
+//         outArray[i] = new Random().Next(minValue, maxValue);
+//     }
+//     return outArray;
+// }
+
+// //Печатаем массив
+// void PrintArray(double[] arr) 
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < arr.Length - 1; i++)
+//     {
+//         Console.Write(arr[i] + ",");
+//     }
+//     Console.Write(arr[arr.Length - 1] + "]");
+// }
+
+// // Метод печати результата в консоль
+// void PrintData(string res, double value)
+// {
+//    Console.WriteLine(res);
+// }
+
+// // Метод вычисления min b max значений
+// void MinMax(double[] arr)
+// {
+//      for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] > maxValue)
+//             maxValue = arr[i];       
+//         if (arr[i] < maxValue)
+//             minValue = arr[i];
+//     }
+// }
+
+// Вариант, исправленный преподавателем
+
+// Задайте массив вещественных чисел. Найдите разницу
+// между максимальным и минимальным элементов массива.
 double[] array = GenArray(5,100,999);
 PrintArray(array);
-double maxValue =  double.MinValue; 
-double minValue =  double.MaxValue; 
-double result = MinMax(maxValue-minValue);
+double maxValue = double.MinValue;
+double minValue = double.MaxValue;
+MinMax(array);
+double result =maxValue-minValue;
 Console.WriteLine();
 PrintData("Разница между макс и мин элементами массива ", result);
 
 // Заполняем массив
 double[] GenArray(int len, int minValue, int maxValue)
 {
-    Random rnd = new Random();
-    double[] outArray = new double[len];
-    for (int i = 0; i < len; i++)
-    {
-        outArray[i] = new Random().Next(minValue, maxValue);
-    }
-    return outArray;
+Random rnd = new Random();
+double[] outArray = new double[len];
+for (int i = 0; i < len; i++)
+{
+outArray[i] = (new Random().Next(minValue, maxValue))+(new Random().NextDouble());
+}
+return outArray;
 }
 
 //Печатаем массив
-void PrintArray(double[] arr) 
+void PrintArray(double[] arr)
 {
-    Console.Write("[");
-    for (int i = 0; i < arr.Length - 1; i++)
-    {
-        Console.Write(arr[i] + ",");
-    }
-    Console.Write(arr[arr.Length - 1] + "]");
+Console.Write("[");
+for (int i = 0; i < arr.Length - 1; i++)
+{
+Console.Write(arr[i] + ",");
+}
+Console.Write(arr[arr.Length - 1] + "]");
 }
 
 // Метод печати результата в консоль
 void PrintData(string res, double value)
 {
-   Console.WriteLine(res);
+Console.WriteLine(res+value);
 }
 
 // Метод вычисления min b max значений
 void MinMax(double[] arr)
 {
-     for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i] > maxValue)
-            maxValue = arr[i];       
-        if (arr[i] < maxValue)
-            minValue = arr[i];
-    }
+for (int i = 0; i < arr.Length; i++)
+{
+if (arr[i] > maxValue)
+maxValue = arr[i];
+
+if (arr[i] < maxValue)
+minValue = arr[i];
+}
 }
---
+++ Sem7Task52/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+// в каждом столбце.
+// Запрос данных пользователяю, принимает строку заголовок.
+Console.WriteLine ("Введите число строк: ");
+int m = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите число столбцов: ");
+int n = Convert.ToInt32 (Console.ReadLine());
+int[,] massive = new int [m,n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        massive[i,j] = new Random().Next (-10, 10);
+        Console.Write (massive[i,j]+" ");
+    }
+    Console.WriteLine();
+}
+// Считает среднее арифметическое по столбцам.
+int result = 0;
+
+for (int j = 0; j < n; j++)
+{
+    result = Average(j);
+    Console.WriteLine(result);
+}
+
+int Average(int column)
+{
+    int average = 0;
+    int x = 0;
+    for (int i = 0; i < m; i++)
+    {
+      average += massive[i, column];
+    }
+       x = average / m;
+    {
+    return x;
+    }
+}
