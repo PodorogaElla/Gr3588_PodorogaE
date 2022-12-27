8858cd4f6f1619c2b09d6edffd20b5fa5bba51a2
PodorogaElla <Elka_forest@mail.ru>
2022-12-27 15:00:25 +0500

    add solution for Tasks 64, 66, 68

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+
+int m = ReadData("Введите число M: ");
+int n = ReadData("Введите число N: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+// Метод рекурсионного суммирования с выводом на печать 
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов= {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int m = int.Parse(Console.ReadLine() ?? "0");
+    return m;
+    
+ }
